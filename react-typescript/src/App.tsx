8d8da55d2fe36e8592e404e74158da132ea4249f
PreetS123 <preetbhardwaj517@gmail.com>
2024-11-09 07:13:15 +0530

    words list json data ready

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import wordList from './json/words.json';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [gussedwords, setGussedWords] = useState(()=>{
+    return wordList[Math.floor(Math.random() * wordList.length)]
+  })
 
   return (
     <>
